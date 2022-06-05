6cc2458bd4b4ef1ab6d7c2361b70c6084c5f36e4
LonerStayle <dlwlstjq410@gmail.com>
2022-06-05 12:15:00 +0900

    [Create] init App

---
+++ app/src/main/java/kr/loner/hoomasjip/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.loner.hoomasjip
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.loner.networkresourcecontrolltrapp.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
